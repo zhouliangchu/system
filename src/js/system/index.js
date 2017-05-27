309d7881abf869d6b91aa5986afd4f709b04c361
yang <2439861222@qq.com>
2017-05-28 07:06:38 +0800

    add

--- src/js/system/index.js
+++ src/js/system/index.js
@@ -1,126 +1,217 @@
 /**
  * Created by Administrator on 2017/5/22.
  */
-var app = angular.module('myapp',['ui.router']);
+
+// 配置一级路由，即头部点击个人中心、系统设置等时跳转不同的路由渲染不同的数据
+// 因为此时页面的格式相同只是数据不同，所以显示的为同一页面不同数据
 var num = 100;
+app.config(function(alldata,$stateProvider){
+    // 遍历数据中第一个数组里面的数据，分别为他们配置不同的路由
+    alldata.fstdata.forEach(function(item){
+        $stateProvider
+            .state({
+                name: item.route,
+                url: '/'+ item.route,
+                templateUrl: 'page/aside.html',
+                // 通过遍历得到所有的路由，不同路由的页面分属不同的控制器
+                controller: function($scope,alldata,$stateParams){
+                    // 从数据中的第二个数组里筛选出parentid与第一个数组的id相同的数据分别进行渲染
+                    var newData = alldata.secdata.filter(function(i){
+                        return i.parentid == $stateParams.self.id
+                    });
+                    // console.log(newData);
+                    // 存放每次筛选出来的数据
+                    $scope.data = newData;
+                    // 侧边栏管理项的题目
+                    $scope.parentName = $stateParams.self.nickname;
 
-app.controller('sysCon', function($scope,alldata,page){
-    $scope.data = alldata.thirdata;
-    // 拿到原始数据并保存到一个函数中，调用此函数即可得到原始数据
-    $scope.fileData = function(){
-        // map方法返回一个新的对象
-        return alldata.thirdata.map(function(i){
-            return i;
-        })
-    };
-
-    // 分页
-    // 定义每一页显示数据的长度
-    $scope.dataNum = 2;
-    // 定义中间页显示的页数，只能为奇数
-    $scope.midPage = 5;
-
-    // 点击添加用户、用户管理时切换当前状态
-    $('nav ul li').on('click', function(){
-        $(this).addClass('current').siblings().removeClass('current');
-    });
-
-    // 接收子控制器派发过来的新添加的数据，并添加到数据中
-    $scope.$on('addDate', function(e,d){
-        $scope.data.push(d.data);
-        page($scope);
-    });
-
-    // 删除用户
-    $scope.remove = function(id){
-        $scope.data.forEach(function(ele,i){
-            if(ele.ID == id){
-                $scope.data.splice(i,1);
-                page($scope);
-                // console.log(i);
-            }
-        })
-    };
-
-    // 修改
-    $scope.update = function(id){
-        $scope.boll = true;
-
-        $scope.data.forEach(function(ele,i){
-
-            if(ele.ID == id){
-                $scope.tar = {};
-                for(k in ele){
-                    $scope.tar[k] = ele[k];
-                }
-            }
-        })
-    };
-
-    // 修改后点击确定
-    $scope.sure = function(){
-        $scope.boll = false;
-
-        $scope.data.forEach(function(ele,index){
-            if(ele.ID == $scope.tar.ID){
-                $scope.data[index] = $scope.tar;
-                page($scope);
-            }
-        })
-    };
-
-    // 修改后点击取消
-    $scope.no = function(){
-        $scope.boll = false;
-        // $scope.cutData = alldata.thirdata;
-    };
-
-    // 调用分页的服务，并将$scope作为参数传递过去
-    page($scope);
-
-});
-
-app.config(function($stateProvider,$urlRouterProvider){
-    $urlRouterProvider.otherwise('/user');
-    $stateProvider
-        .state({
-            name: 'add',
-            url: '/add',
-            templateUrl: 'page/add.html',
-            controller: function($scope){
-                $scope.add = {};
-                // 添加用户
-                $scope.addFn = function(){
-                    // 用户的创建时间
-                    var today = new Date();
-                    var y = today.getFullYear();
-                    var M = today.getMonth();
-                    var d = today.getDate();
-                    var h = today.getHours();
-                    var m = today.getSeconds();
-                    var time = y+'-'+change(M)+'-'+change(d)+' '+change(h)+':'+change(m);
-                    function change(num){
-                        if(num<10){
-                            num = '0'+num;
-                        }
-                        return num
+                    // 控制侧边栏管理项的显示隐藏
+                    $scope.listShow = true;
+                    $scope.addShow = false;
+                    // 点击侧边栏题目出现管理项事件
+                    $scope.showFn = function(){
+                        $scope.listShow = !$scope.listShow;
+                        $scope.addShow = !$scope.addShow;
                     }
-
-                    $scope.add.ID = num++;
-                    $scope.add.creattime = time;
-
-                    // 将新添加的数据派发给父控制器
-                    $scope.$emit('addDate', {
-                        data: $scope.add
-                    })
+                },
+                // 将第一个数组里的每一个对象作为参数传递，通过判断id与第二个数组里的parentid对应来渲染相应的内容
+                params: {
+                    self: item
                 }
-            }
-        })
-        .state({
-            name: 'user',
-            url: '/user',
-            templateUrl: 'page/user.html'
-        })
+            })
+    });
+
+    // 遍历第二个数组里面的数据，配置二级路由，显示每项管理的详细页面
+    alldata.secdata.forEach(function(i){
+        $stateProvider
+            .state({
+                name: i.route,
+                url: '/'+ i.route,
+                templateUrl: 'page/'+ i.enName+'.html',
+                controller: function($scope,alldata,page,$filter,$rootScope){
+
+                    // 用户管理
+                    var nameFn = function(message){
+                       // $scope.data = alldata.thirdata;
+                        $scope.fileData = function(){
+                            // 过滤后返回一个新的对象
+                            return $filter('filter')(message,{role:$scope.sysRole,state:$scope.statues,loginname:$scope.loginName})
+                        };
+
+                        // 分页
+                        // 定义每一页显示数据的长度
+                        $scope.dataNum = 2;
+                        // 定义中间页显示的页数，只能为奇数
+                        $scope.midPage = 5;
+
+                        // 调用分页的服务，并将$scope作为参数传递过去
+                        page($scope);
+                        $scope.pageShow(1);
+                        // 点击查询进行筛选
+                        $scope.searchFn=function () {
+                            $scope.pageShow(1);
+                        };
+
+                        //  删除
+                        // 控制弹出框的显示隐藏，最初先隐藏起来
+                        $scope.popupShow = false;
+                        // 点击删除时显示弹出框
+                        $scope.remove=function (item) {
+                            $scope.popupShow = true;
+                            // 点击确定，弹出框消失，同时进行删除
+                            $scope.sureFn = function(){
+                                $scope.popupShow = false;
+                                var indexPage;
+                                // 点击查询时从筛选出的数据中定位页码，使查询后删除后页面还停留在当前页  $scope.fileData() 进行筛选后的数据
+                                $scope.fileData().forEach(function(i,index){
+                                    switch (i.ID){
+                                        case item.ID:{
+                                            indexPage= Math.ceil(index/$scope.dataNum);
+                                            // 判断当删除的为第一页第一条数据时仍留在本页
+                                            if(indexPage == 0){
+                                                indexPage = 1;
+                                            }
+                                        }
+                                    }
+
+                                });
+
+                                // $scope.data原始数据，删除时要将数据从原始数据中删除，然后需重新调用分页服务
+                                message.forEach(function (i,index) {
+                                    switch (i.ID){
+                                        case item.ID:{
+                                            message.splice(index,1);
+                                        }
+                                    }
+                                });
+
+                                // console.log(indexPage)
+                                $scope.pageShow(indexPage);
+                            };
+
+                            // 点击取消弹出框消失，同时保留数据不进行删除
+                            $scope.noFn = function(){
+                                $scope.popupShow = false;
+                            }
+
+                        };
+
+                        // 修改
+                        $scope.boll = false; // 控制修改弹出框的显示隐藏
+                        $scope.update = function(item){
+                            $scope.boll = true;
+                            $scope.start = '启用';
+                            $scope.stop = '禁用';
+                            $scope.changeState = item.state;
+                            // 通过ng-value 和 ng-model 来监测单选框的选中项，ng-value 的值等于 ng-model 的值的那一项即为选中项
+
+                            // 将进行修改的数据传送给修改框
+                            $scope.tar = {};
+                            for(k in item){
+                                $scope.tar[k] = item[k];
+                            }
+
+                            // 选择的状态发生改变事件
+                            $scope.changeFn = function(val){
+                                // 将选中那一项的值赋给ng-model
+                                $scope.changeState = val;
+                            };
+                            // 修改后点击确定
+                            $scope.sure = function(){
+                                $scope.boll = false;
+
+                                message.forEach(function(ele,index){
+                                    // 判断ID相等时将修改后的值保存至原始数据中
+                                    if(ele.ID == $scope.tar.ID){
+                                        for(i in $scope.tar){
+                                            message[index][i] = $scope.tar[i]
+                                        }
+
+                                        // 将原数据中的状态改为修改后的状态
+                                        message[index].state = $scope.changeState;
+                                    }
+                                });
+                            };
+
+                            // 修改后点击取消
+                            $scope.no = function(){
+                                $scope.boll = false;
+                            };
+                            page($scope);
+                        };
+                    };
+
+                    // 添加用户
+                    var addNameFn = function(){
+                        $scope.data = alldata.thirdata;
+                        // 定义一个空对象，用来存放添加用户的信息
+                        $rootScope.add = {};
+
+                        // 用户的创建时间
+                        var today = new Date();
+                        var y = today.getFullYear();
+                        var M = today.getMonth()+1;
+                        var d = today.getDate();
+                        var h = today.getHours();
+                        var m = today.getSeconds();
+                        var time = y+'-'+change(M)+'-'+change(d)+' '+change(h)+':'+change(m);
+
+                        function change(n){
+                            if(n<10){
+                                n = '0'+n;
+                            }
+                            return n
+                        }
+
+                        $rootScope.add.ID = num++;
+                        $rootScope.add.creattime = time;
+
+                        $scope.addName = function(){
+                            $scope.data.push($rootScope.add);
+                            alert('用户添加成功');
+                        }
+                    };
+
+                    switch (i.id){
+                        case 23:{
+                            $scope.data = alldata.thirdata;
+                            // 用户管理
+                            nameFn($scope.data);
+                        }break;
+                        case 22:{
+                            // 角色管理
+                            $scope.data=alldata.fourdata;
+                            nameFn($scope.data);
+                        }break;
+                        case 21:{
+                            addNameFn()
+                        }break;
+                    }
+                }
+            })
+    })
 });
+
+
 
 
--- src/js/system/service.js
+++ src/js/system/service.js
@@ -1,35 +1,74 @@
-/**
- * Created by Administrator on 2017/5/25.
- */
+/** Created by Administrator on 2017/5/26.*/
+
+// 将头部封装成一个指令
+app.directive('headers', function () {
+    return {
+        restrict: 'ECMA',
+        replace: false,
+        templateUrl: 'page/header.html',
+        controller: function($scope,alldata){
+            $scope.data = alldata.fstdata;
+        }
+    }
+});
+
+// 封装一个删除时确定、取消的弹出框指令
+app.directive('removeSure', function(){
+    return {
+        restrict: 'E',
+        replace: false,
+        templateUrl: 'page/dialog/remove.html'
+    }
+});
+
+// 封装修改时的指令
+app.directive('updateSure', function(){
+    return {
+        restrict: 'E',
+        replace: false,
+        templateUrl: 'page/dialog/update.html'
+    }
+});
+
+// 封装角色修改时的指令
+app.directive('roleSure', function(){
+    return {
+        restrict: 'E',
+        replace: false,
+        templateUrl: 'page/dialog/roleUpdate.html'
+    }
+});
+
 // 封装一个分页的服务
 app.service('page', function(){
     return function($scope){
         // 将$scope作为一个参数传入到服务中，此服务将能使用控制其中的所有属性和方法
         // console.log($scope.fileData());
 
-        // 重新定义一个变量用来存储原始数据
-        var oldData = $scope.fileData();
-        // 得到页数 = Math.ceil(数据总长度 / 每页显示的数据长度)
-        $scope.allPage = Math.ceil(oldData.length/$scope.dataNum);
-        // 定义一个数组用来存放页数，好渲染至页面，存放的页数为第二页至倒数第二页
-        $scope.pageArr = [];
-        for(var i=2;i<$scope.allPage;i++){
-            $scope.pageArr.push(i);
-        }
-
-        // 点击每一页通过页码判断所要显示的数据
-        $scope.changeIndex = function(i){
-            $scope.pageShow(i);
-            $('.page input').val(i);
-        };
-
         // 当前显示哪几页
         $scope.pageShow = function(i){
+            // 重新定义一个变量用来存储原始数据
+            var oldData = $scope.fileData();
+            // 得到页数 = Math.ceil(数据总长度 / 每页显示的数据长度)
+            $scope.allPage = Math.ceil(oldData.length/$scope.dataNum);
+            // 定义一个数组用来存放页数，好渲染至页面，存放的页数为第二页至倒数第二页
+            $scope.pageArr = [];
+            for(var j=2;j<$scope.allPage;j++){
+                $scope.pageArr.push(j);
+            }
+
+            // 点击每一页通过页码判断所要显示的数据
+            $scope.changeIndex = function(j){
+                $scope.pageShow(j);
+                $('.page input').val(j);
+            };
             // 通过下标和点击事件控制显示的当前页 i代表当前页页码 $scope.index 为当前页的下标
             $scope.index = i-1;
             // 控制前后……的显示隐藏
             $scope.topMore = false;
             $scope.lastMore = false;
+            $scope.lastBoll = true;
+            $scope.pageS = true;
 
             // 判断要显示哪几页页码以及前后……
             // 显示的页码中中间页左右各显示几页
@@ -75,9 +114,19 @@ app.service('page', function(){
                 }
             }
 
+            // 当总页数在6页以下时，隐藏……
             if($scope.allPage <= 6){
                 $scope.topMore = false;
                 $scope.lastMore = false;
+            }
+
+            // 当页数为1页以下时把最后一页隐藏，仅留第一页
+            if($scope.allPage <= 1){
+                $scope.lastBoll = false;
+                // 当页数<=0即无数据显示时，隐藏分页
+                if($scope.allPage <= 0){
+                    $scope.pageS = false;
+                }
             }
 
             // 每一页要显示的数据
@@ -94,7 +143,7 @@ app.service('page', function(){
 
         // 当输入框的值改变时跳至相应页数
         $scope.changeValue = function(){
-            $scope.value = $('.page input').val();
+            $scope.value = Number($('.page input').val());
             $scope.pageShow($scope.value);
         };
 
